72423a13e49eabfb268dd52757b414930e72581e
Aidan <atakishievaaidan@mail.ru>
2021-02-07 23:38:52 +0400

    07.02

--- js/main.js
+++ js/main.js
@@ -124,3 +124,34 @@ $(document).mouseup(function (e) {
       $('.custom-drop-down').hide()
       $(this).siblings('.custom-drop-down').toggle()
   });
+
+
+  function myFunction() {
+    var x = document.getElementById("add_address");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
